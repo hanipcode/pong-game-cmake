9152a40c48d18bd7c15235f2d4bed0bc2168a0b2
Muhammad Hanif <hanifeij@yahoo.co.id>
2024-12-22 15:46:57 +0700

    feat: initial commit

--- _deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hanifmuhammad/Projects/pong-game-cmake/_deps/raylib-src
-# Build directory: /Users/hanifmuhammad/Projects/pong-game-cmake/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
